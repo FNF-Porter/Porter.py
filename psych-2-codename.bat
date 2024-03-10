86889bf5ccb2e73dc9845a1a9bf0fa1b8633d379
Gusborg <91546896+gusborg88@users.noreply.github.com>
2024-03-10 13:21:32 -0500

    using robocopy instead of xcopy and some other changes

---
+++ psych-2-codename.bat
@@ -0,0 +1,41 @@
+::@echo off
+
+::variables that are the same for every mod
+set hyphens=-------------------------------
+set difficulties="easy" "normal" "hard"
+title FNF Porter v1
+if exist "*.exe" (
+    color 04
+    echo Whoops! No .exe mods. /mods folder mods only.
+    pause 
+) else ( 
+    echo Hello Bro
+    echo Please place this .bat in the same folder as your mod
+    echo %hyphens%
+    set /p modName=What's the mods name? No spaces or weird characters
+)
+
+::folders that are the same in psych and codename. Shoutout to this website for this script: https://www.tutorialspoint.com/batch_script/batch_script_arrays.htm
+for /D %%A in (fonts,music,shaders,sounds,images,videos) do (
+    robocopy %%A %modName%\%%A
+)
+
+
+robocopy weeks\ %modName%\data\weeks\weeks\
+
+robocopy /s songs\ %modName%\songs\
+
+for %%A in (%modName%\songs\) do (
+    robocopy /s *.ogg song\
+)
+
+for %%A in (data\*\.json) do (
+    echo %%A
+)
+
+echo %hyphens%
+echo            Done!             
+echo You can close this window now
+pause
+
+rd /q /s %modName%
