76bff0c2ab674ec98059e4a3a1054878632bd7fe
Gusborg <91546896+gusborg88@users.noreply.github.com>
2024-03-10 14:19:29 -0500

    wheres the show diff button when i need it
    I forgot what the differences are

--- psych-2-codename.bat
+++ psych-2-codename.bat
@@ -1,8 +1,9 @@
-@echo off
+::@echo off
 
 ::variables that are the same for every mod
 set hyphens=-------------------------------
 set difficulties="easy" "normal" "hard"
+set modName=%~dp0
 title FNF Porter v1
 if exist "*.exe" (
     color 04
@@ -11,22 +12,26 @@ if exist "*.exe" (
 ) else ( 
     echo Hello Bro
     echo Please place this .bat in the same folder as your mod
+    echo for example
+    echo 17 bucks
+    echo    characters
+    echo    data
+    echo    images
+    echo    ...
+    echo    %~n0%~x0
     echo %hyphens%
-    set /p "modName=What's the mods name? No spaces or weird characters"
 )
 
 ::folders that are the same in psych and codename. Shoutout to this website for this script: https://www.tutorialspoint.com/batch_script/batch_script_arrays.htm
 for /D %%A in (fonts,music,shaders,sounds,images,videos) do (
-    robocopy %%A "%modName%\%%A"
+    robocopy %%A "%modName%"\%%A
 )
 
 
-robocopy weeks\ "%modName%\data\weeks\weeks\"
+robocopy weeks\ "%modName%"\data\weeks\weeks\
 
-robocopy /s songs\ "%modName%\songs\"
-
-for %%A in ("%modName%\songs\") do (
-    robocopy /s *.ogg song\
+for %%A in (songs\) do (
+    robocopy /s songs\ "%modName%"\songs\songName\song
 )
 
 for %%A in (data\*\.json) do (
@@ -38,4 +43,4 @@ echo            Done!
 echo You can close this window now
 pause
 
-::rd /q /s %modName%
+rd /q /s "%modName%"
