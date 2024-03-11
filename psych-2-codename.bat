164aca9a87afaed9a60914a82460c88607f4e9ba
Gusborg <91546896+gusborg88@users.noreply.github.com>
2024-03-11 13:33:09 -0500

    Why the fuck doesnt it work

--- psych-2-codename.bat
+++ psych-2-codename.bat
@@ -11,32 +11,53 @@ dir /AD /B mods\
 echo %hyphens%
 set /p "modName=What mod would you like to port? Enter the exact folder name: "
 
-
-
-set output=psych-to-codename-output\%modName%
+set output=output\%modName%
 set input=mods\%modName%
+
+robocopy %input%\images\menucharacters\*.xml %output%\data\weeks\characters
+robocopy %input%\images\noteSplashes\*.xml %output%\data\splashes\
 
 ::folders that are the same in psych and codename
 for /D %%A in (fonts,music,shaders,sounds,images,videos) do (
-    robocopy /s %input%\%%~A %output%\%%~A
+    robocopy /s %input%\%%A %output%\%%A
 )
 
 
 robocopy %input%\weeks %output%\data\weeks\weeks\
 
-for %%A in (.) do (
-    robocopy /s %input%\songs\ %output%\songs\
-    robocopy /s %input%\weeks\ %output%\data\weeks\week\
+robocopy %input%\characters %output%\data\characters
+
+::for %%A in (%output%\data\characters\) do (
+::    python json-to-xml.py %modName%
+::)
+
+robocopy /s %input%\songs\ %output%\songs\
+
+for /D %%A in (%output%\songs\*) do (
+    for %%B in (%%A\*) do (
+        :: thank you chatgpt
+        rem Extract the file name and extension
+        set filename=%%~nxB
+        rem Create a new directory with the file name (without extension)
+        mkdir %%A\!filename:~0,-4!" >nul 2>&1
+        rem Move the file to the newly created directory
+        move %%B "%%A\!filename:~0,-4!\" >nul 2>&1
+    )
 )
 
-for /D %%A in (.) do (
-    robocopy /s /mov %output%\songs\ %output%\songs\song\ *.ogg
-)
+::for /D %%A in (%output%\songs\) do (
+::    ren %output%\songs\%%A\ 
+::)
+
+::for /D %%A in (.) do (
+::    robocopy /s /mov %output%\songs\ %output%\songs\song\ *.ogg
+::)
 
 cd %output%
-echo Ported with FNF Porter v1. By Gusborg and BombasticTom. Download for yourself: https://gamebanana.com/mods/ > fnf-porter.txt
+echo Ported with FNF Porter v1. By Gusborg and BombasticTom. Download: https://gamebanana.com/mods/ > fnf-porter.txt
 cd ..\..\
 
 echo %hyphens%
 echo Done!
+echo Check output/ for your mod
 pause
