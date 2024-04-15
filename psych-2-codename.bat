76b1fee29fd86a9f8320586edc63cce076f26b99
Gusborg <gusulioche@gmail.com>
2024-04-14 20:15:01 -0500

    made it better (im leaving some of the old lines in to reference later)

--- psych-2-codename.bat
+++ psych-2-codename.bat
@@ -1,7 +1,10 @@
+::Created by Gusborg and BombasticTom. Read LICENSE
 ::@echo off
 
 ::variables that are the same for every mod
 set hyphens=---------------------------------------
+cd ..
+set workingDir=%cd%
 
 title FNF Porter v1 
 echo FNF Porter v1
@@ -12,46 +15,63 @@ echo Your mods:
 dir /AD /B mods\
 set /p "modName=What mod would you like to port? Enter the exact folder name: "
 
-set output=output\%modName%
-set input=mods\%modName%
+set "input=%workingDir%\mods\%modName%"
+md %workingDir%\mods-cne\%modName%\
+set "output=%workingDir%\mods-cne\%modName%"
+set "log=%output%\fnf-porter.log"
+echo Ported with FNF Porter v1. By Gusborg and BombasticTom.>%log%
+echo Download: https://gamebanana.com/mods/>>%log%
+echo %hyphens%>>%log%
+echo ERRORS:>>%log%
 
+:: there's probably some way to do this with a for loop but idk how
+call :copy-generic fonts fonts
+call :copy-generic music music
+call :copy-generic shaders shaders
+call :copy-generic sounds sounds
+call :copy-generic images images
+call :copy-generic videos videos
 
-robocopy %input%\images\menucharacters\*.xml %output%\data\weeks\characters
-robocopy %input%\images\noteSplashes\*.xml %output%\data\splashes\
+::robocopy %input%\images\menucharacters\*.xml %output%\data\weeks\characters
+::robocopy %input%\images\noteSplashes\*.xml %output%\data\splashes\
 
-::folders that are the same in psych and codename
-for /D %%A in (fonts,music,shaders,sounds,images,videos) do (
-    robocopy /s %input%\%%A\ %output%\%%A\
-)
+::to check when all the calls end
+echo I shit myself
+echo the shit is dripping donw my legs
 
+::robocopy %input%\weeks %output%\data\weeks\weeks\
 
-robocopy %input%\weeks %output%\data\weeks\weeks\
-
-robocopy %input%\characters %output%\data\characters
+::robocopy %input%\characters %output%\data\characters
 
 ::for %%A in (%output%\data\characters\) do (
 ::    python json-to-xml.py %modName%
 ::)
 
-robocopy %input%\songs\ %output%\songs\ /xf *.json /s
-cd %output%\songs\
-for /D %%A in (*) do (
-    robocopy /MOV %%A\ %%A\song\
+::robocopy %input%\songs\ %output%\songs\ /xf *.json /s
+::cd %output%\songs\
+::for /D %%A in (*) do (
+::    robocopy /MOV %%A\ %%A\song\
+::)
+::cd ..
+
+::robocopy %input%\data\ %output%\songs\ /xf credits.txt /s
+::cd %output%\songs\
+::for /D %%A in (*) do (::
+::    robocopy /MOV %%A\ %%A\charts\
+::)
+::cd ..
+
+goto :eof
+:copy-generic
+robocopy /s /unilog+:%log% %input%\%~1 %output%\%~2
+if %errorlevel% geq 8 (
+    echo ERROR: %1 couldn't copy to %2. Read error above>>%log%
+) else (
+    echo %1 successfully copied to %2>>%log%
 )
-cd ..
-
-robocopy %input%\data\ %output%\songs\ /xf credits.txt /s
-cd %output%\songs\
-for /D %%A in (*) do (
-    robocopy /MOV %%A\ %%A\charts\
-)
-cd ..
-
-cd %output%
-echo Ported with FNF Porter v1. By Gusborg and BombasticTom. Download: https://gamebanana.com/mods/ > fnf-porter.txt
-cd ..
-
+exit /b
+xcopy 
 echo %hyphens%
 echo Done!
-echo Check output/ for your mod
+echo Check "%output%" for your mod
 pause
---
+++ unused/readme.txt
@@ -0,0 +1,2 @@
+stuff to delete before releasing
+Also delete README.md and LICENSE
