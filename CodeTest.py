bf4f5cfb53880747dc160b13b8629fb6e4b05043
BombasticTom <85838311+BombasticTom@users.noreply.github.com>
2024-04-27 19:27:32 +0200

    actually remove CodeTest (im dumb)

--- CodeTest.py
+++
@@ -1,7 +0,0 @@
-from src.XMLTools import WeekXML
-from src.Paths import Paths
-
-Paths.assetsDir = "./mods/btoad-fnf"
-
-# print(Paths.parseJson("weeks\story1Btoad"))
-test = WeekXML().setup("weeks\story1Btoad")
